6c7e299ca2bb35256f59dedbc9eaae1fc0e3cbcd
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-02-10 23:31:51 +0100

    building the home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,19 @@
+import Feed from '@/components/Feed';
+
 export default function Home() {
     return (
-        <div>Welcome to the Homepage of our application</div>
+        <section className="w-full flex-col flex-center">
+            <h1 className="head_text text-center">
+                Discover $ Share
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">AI-Powered Prompts</span>
+            </h1>
+
+            <p className="text-center desc">
+                Prompt-App is an open source AI prompting tool for modern world to discover, create and share creative prompt
+            </p>
+
+            <Feed />
+        </section>
     )
 }
---
+++ components/Nav.jsx
@@ -0,0 +1,51 @@
+"use client"
+
+import Image from "next/image"
+import Link from "next/link"
+// import { signIn, signOut, useSession, getProviders } from "next-auth/react"
+import { useState, useEffect } from "react"
+
+
+export default function Nav() {
+    const isUserLoggedIn = true;
+
+    return (
+        <nav className="flex-between mb-16 pt-3 w-full">
+            <Link href="/" className="flex gap-2 flex-center">
+                <Image
+                    src="/assets/images/logo.svg"
+                    alt="Prompt-app logo"
+                    width={30}
+                    height={30}
+                    className="object-contain"
+                />
+                <p className="logo_text">Prompt-App</p>
+            </Link>
+
+            {/* Desktop Navigation */}
+            <div className="sm:flex hidden">
+                {isUserLoggedIn ? (
+                    <div className="flex gap-3 md:gap-5">
+                        <Link href="/create-prompt" className="black_btn">
+                            Create prompt
+                        </Link>
+                        <button type="button" className="outline_btn">
+                            Sign Out
+                        </button>
+                        <Link href="/profile">
+                            <Image
+                                src="/assets/images/logo.svg"
+                                alt="Account profile"
+                                width={37}
+                                height={37}
+                                className="rounded-full"
+                            />
+                        </Link>
+                    </div>
+                ): (
+                  <></>      
+            )}
+            </div>
+        </nav>
+    )
+}

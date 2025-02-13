30cd716cd2c02d1ac20277684aa3c77f38c2f0fa
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-02-14 00:23:07 +0100

    instilling next auth and mongoDB continues

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,18 +7,18 @@ import { useState, useEffect } from "react"
 
 
 export default function Nav() {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession();
 
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
-        const setProviders = async () => {
+        const setUpProviders = async () => {
             const response = await getProviders();
 
             setProviders(response);
         }
-        setProviders();
+        setUpProviders();
     }, [])
 
     return (
@@ -34,9 +34,11 @@ export default function Nav() {
                 <p className="logo_text">Prompt-App</p>
             </Link>
 
+            
+
             {/* Desktop Navigation */}
             <div className="sm:flex hidden">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex gap-3 md:gap-5">
                         <Link href="/create-prompt" className="black_btn">
                             Create prompt
@@ -73,7 +75,7 @@ export default function Nav() {
 
             {/* Mobile Navigation */}
             <div className="sm:hidden flex relative">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex">
                         <Image
                             src="/assets/images/logo.svg"
